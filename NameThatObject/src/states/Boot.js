6fb50487b2f1c0eca08ba89e9c2300d6dd2b75d2
conradolk <conrado.lk@gmail.com>
2017-02-16 23:39:45 -0300

    Added image, sound texture loading and alignment

--- NameThatObject/src/states/Boot.js
+++ NameThatObject/src/states/Boot.js
@@ -3,9 +3,11 @@ import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init () {
-    this.stage.backgroundColor = '#EDEEC9'
-    this.fontsReady = false
-    this.fontsLoaded = this.fontsLoaded.bind(this)
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.refresh();
+    this.stage.backgroundColor = '#EDEEC9';
+    this.fontsReady = false;
+    this.fontsLoaded = this.fontsLoaded.bind(this);
   }
 
   preload () {
@@ -16,23 +18,25 @@ export default class extends Phaser.State {
       active: this.fontsLoaded
     })
 
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'Loading resouces', { font: '16px Arial', fill: '#000', align: 'center' })
-    text.anchor.setTo(0.5, 0.5)
+    let text = this.add.text(this.world.centerX, this.world.centerY, 'Loading resouces', { font: '16px Arial', fill: '#000', align: 'center' });
+    text.anchor.setTo(0.5, 0.5);
 
-    this.load.image('loaderBg', './assets/images/loader-bg.png')
-    this.load.image('loaderBar', './assets/images/loader-bar.png')
-    this.load.image('gametitle', './assets/images/gametitle.png')
+    this.load.image('loaderBg', './assets/images/loader-bg.png');
+    this.load.image('loaderBar', './assets/images/loader-bar.png');
+    this.load.image('gametitle', './assets/images/gametitle.png');
+    this.load.atlas('menubuttons', './assets/images/bluesheet.png', './assets/images/bluesheet.json');
+    this.load.audio('clicksound', './assets/audio/click.ogg');
 
   }
 
   render () {
     if (this.fontsReady) {
-      this.state.start('Menu')
+      this.state.start('Menu');
     }
   }
 
   fontsLoaded () {
-    this.fontsReady = true
+    this.fontsReady = true;
   }
 
 }
