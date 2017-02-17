256ee82e25de8d9246fc00371be61c90b9ecb333
conradolk <conrado.lk@gmail.com>
2017-02-16 23:42:12 -0300

    Added atlas spritesheet loading, click sound..
    Added horizontal alignment

--- NameThatObject/src/states/Menu.js
+++ NameThatObject/src/states/Menu.js
@@ -5,35 +5,43 @@ var about;
 export default class extends Phaser.State {
 
   init() {
+
+    //  Horizontal alignment
+    this.game.scale.pageAlignHorizontally = true;
+    this.game.scale.refresh();
+
     //  About information text
     about = game.add.text(this.world.centerX, this.world.centerY + 120,"Name that object: game made for Ubykuo   ", {font:"Bangers", fontSize: 22,fill:"#000",align:"center"});
     about.visible = false;
     about.anchor.setTo(0.5, 0.5);
+
   }
 
   preload () {
-    //  Images and sprites
+    //  Sprites
     game.load.image('volumeon', 'assets/images/volumeon.png');
     game.load.image('menubutton', 'assets/images/menubutton.png');
     game.load.image('whitesquare', 'assets/images/whitesquare.png');
     game.load.image('dog', 'assets/images/dog.png');
     game.load.image('house', 'assets/images/house.png');
     game.load.image('happycar', 'assets/images/happycar.png');
-    game.load.image('bluebuttondown', 'assets/images/bluebuttondown.png');
 
+    //  Spritesheets
     game.load.spritesheet('letter','assets/images/alphabetspritesheet.png',150,212);
 
-    //  Sound
+    //  Sounds
     game.load.audio('levelcompleted','assets/audio/levelcompleted.mp3');
     game.load.audio('error','assets/audio/error.mp3');
-    game.load.audio('youwon','assets/audio/youwon.mp3')
+    game.load.audio('youwon','assets/audio/youwon.mp3');
 
     //  Music
+    game.load.audio('musiclowq','assets/audio/musiclowq.mp3');
 
   }
   
   create () {
 
+    var clickSound = game.add.audio('clicksound');
     //  Game title 
     var title = game.add.sprite(game.world.centerX,game.world.centerY-500 ,'gametitle');
     title.anchor.set(0.5);
@@ -41,16 +49,18 @@ export default class extends Phaser.State {
 
     //  Play button
     this.createButton(game, this.world.centerX, this.world.centerY, 200, 50, 'Play Game', function () { 
+      clickSound.play();
       this.state.start('Game')});
 
     //  About button
     this.createButton(game, this.world.centerX, this.world.centerY + 60, 200, 50, 'About', function () {
+      clickSound.play();
       about.visible ? about.visible = false : about.visible = true;
     });    
   }
 
   createButton(game, x , y, w, h, title, callback) {
-    var button = game.add.button(x, y,'bluebuttondown', callback, this, 2, 1, 0);
+    var button = game.add.button(x, y,'menubuttons', callback, this, 'over', 'up', 'over');
     button.anchor.setTo(0.5,0.5);
     button.height = h;
     button.width = w;
